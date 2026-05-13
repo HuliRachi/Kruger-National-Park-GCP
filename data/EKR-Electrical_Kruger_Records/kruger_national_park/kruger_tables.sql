b8941f077aa9ed81f7f3faab154713b523811323
hulisani <rachyhuly17@gmail.com>
2026-05-13 18:25:12 +0200

    fix: fixed contrains for kruger mysql db

--- data/EKR-Electrical_Kruger_Records/kruger_national_park/kruger_tables.sql
+++ data/EKR-Electrical_Kruger_Records/kruger_national_park/kruger_tables.sql
@@ -1,60 +1,62 @@
+
 create table accommodation(
-    AccID           nvarchar(50)  not null,
-    CampID          nvarchar(50)  not null,
-    UnitType        nvarchar(50)  not null,
-    MaxOccupancy    int           not null,
-    BasePrice       money         not null,
+    AccID           varchar(50)  not null,
+    CampID          varchar(50)  not null,
+    UnitType        varchar(50)  not null,
+    MaxOccupancy    int          not null,
+    BasePrice       decimal(10,2) not null,
     constraint pk_accommodation primary key (AccID)
 );
 
 create table billing(
-    TransID             nvarchar(50)    not null,
-    EntryID             nvarchar(50)    not null,
-    VisitorID           nvarchar(50)    not null,
-    GuideID             nvarchar(50),
-    AccID               nvarchar(50)    not null,
-    VisitDate           date            not null,
-    ConservationFee     money           not null,
-    AccommFee           money           not null,
-    ActivityFee         money           not null,
-    TotalAmount         money           not null,
-    PaymentType         nvarchar(50)    not null,
-    InsertDate          datetime2        not null,
+    TransID             varchar(50)    not null,
+    EntryID             varchar(50)    not null,
+    VisitorID           varchar(50)    not null,
+    GuideID             varchar(50),
+    AccID               varchar(50)    not null,
+    VisitDate           date           not null,
+    ConservationFee     decimal(10,2)  not null,
+    AccommFee           decimal(10,2)  not null,
+    ActivityFee         decimal(10,2)  not null,
+    TotalAmount         decimal(10,2)  not null,
+    PaymentType         varchar(50)    not null,
+    InsertDate          TIMESTAMP      not null,
     constraint pk_billing primary key (TransID)
 );
 
+
 create table field_guides(
-    GuideID         nvarchar(50)    not null,
-    FirstName       nvarchar(50)    not null,
-    LastName        nvarchar(50)    not null,
-    Specialization  nvarchar(50)    not null,
-    CampID          nvarchar(50)    not null,
-    PermitNo        nvarchar(50)    not null,
+    GuideID         varchar(50)    not null,
+    FirstName       varchar(50)    not null,
+    LastName        varchar(50)    not null,
+    Specialization  varchar(50)    not null,
+    CampID          varchar(50)    not null,
+    PermitNo        varchar(50)    not null,
     constraint pk_field_guides primary key (GuideID)    
 );
 
 create table park_entries(
-    EntryID        nvarchar(50)     not null,
-    VisitorID      nvarchar(50)     not null,
-    EntryDate      datetime2        not  null,
-    ExitDate       datetime2        not null,
-    EntryGateID    nvarchar(20)     not null,
-    AccID          nvarchar(20)     not null,
-    VehicleReg     nvarchar(50)     not null,
-    InsertedDate   datetime2        not null,
-    ModifiedDate   datetime2        not null,
+    EntryID        varchar(50)     not null,
+    VisitorID      varchar(50)     not null,
+    EntryDate      TIMESTAMP        not  null,
+    ExitDate       TIMESTAMP        not null,
+    EntryGateID    varchar(20)     not null,
+    AccID          varchar(20)     not null,
+    VehicleReg     varchar(50)     not null,
+    InsertedDate   TIMESTAMP        not null,
+    ModifiedDate   TIMESTAMP        not null,
     constraint pk_park_entries primary key (EntryID)
 );
 
 create table visitors(
-    VisitorID       nvarchar(50)        not null,
-    FirstName       nvarchar(30)        not null,
-    LastName        nvarchar(30)        not null,
-    ID_Passport_No  nvarchar(50)        not null,
-    Nationality     nvarchar(50)        not null,
+    VisitorID       varchar(50)        not null,
+    FirstName       varchar(30)        not null,
+    LastName        varchar(30)        not null,
+    ID_Passport_No  varchar(50)        not null,
+    Nationality     varchar(50)        not null,
     WildCardMember  char(1)             not null,
     DOB             date                not null,
-    ContactNo       nvarchar(30)        not null,
-    ModifiedDate    datetime2           not null,
+    ContactNo       varchar(30)        not null,
+    ModifiedDate    TIMESTAMP           not null,
     constraint pk_visitors primary key (VisitorID)
 );
